1f5cb0105d2fac92326a5b416546ff173de3aae3
Sergi <sergi.prof@hotmail.com>
2024-10-28 11:58:14 +0100

    feat: add Conditionals class

--- src/main/java/org/SergiFactoriaF5/Conditionals.java
+++ src/main/java/org/SergiFactoriaF5/Conditionals.java
@@ -1,6 +1,12 @@
-// imprimir el usuario esta activo si esta activo
-// imprimir el usuario NO esta activo si no esta activepackage org.SergiFactoriaF5;
+package org.SergiFactoriaF5;
 
 public class Conditionals {
-
+    public static void main(String[] args){
+        boolean isActive=true;
+        if (isActive){
+            System.out.println("The user is active");
+        } else {
+            System.out.println("The user is not active");
+        }
+    }
 }
