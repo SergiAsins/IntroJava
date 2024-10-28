35d4a4d89fec5721da969e512966440382121508
Sergi <sergi.prof@hotmail.com>
2024-10-28 16:12:18 +0100

    feat: add Car class

---
+++ src/main/java/org/SergiFactoriaF5/coche/Car.java
@@ -0,0 +1,17 @@
+package org.SergiFactoriaF5.coche;
+
+public class Car {
+    private String brand;
+    private String model;
+    private int year;
+    private double kilometers;
+
+    //constructor
+    public Car(String brand, String model, int year){
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    };
+
+    //
+}
